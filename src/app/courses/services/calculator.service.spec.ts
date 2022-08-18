9fafa6a6072906c2f320f04b8c9a35210000448a
puru531 <purushottam1619@gmail.com>
2022-08-19 00:16:23 +0530

    calculator service

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,5 +1,6 @@
+import { TestBed } from "@angular/core/testing";
 import { CalculatorService } from "./calculator.service";
-// import { LoggerService } from "./logger.service";
+import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
 
@@ -8,10 +9,23 @@ describe('CalculatorService', () => {
 
     beforeEach(() => {
         loggerSpy = jasmine.createSpyObj('LoggerService', ["log"])
-        calculator = new CalculatorService(loggerSpy);
+
+        //As we use dependency injection in Angular we use same notion in test suite, this is helpful when we test components
+        //TestBed allow us to provide dependency services by using dependency injection instead of calling constructors explicitely
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                {provide: LoggerService, useValue: loggerSpy}
+            ]
+        })
+
+        // calculator = new CalculatorService(loggerSpy);
+        //use TestBed to retrieve Calculator Service, instead of caaling constructors explicitely, by using dependency injection as a unique dependency injection key identifier
+        calculator = TestBed.get(CalculatorService);
     })
 
-    it('should add two numbers', () => {
+    //Add f before any test spec or test suite to focus test of that block and bypass rest
+    fit('should add two numbers', () => {
         // const logger = new LoggerService()
         // spyOn(logger, 'log');
         
@@ -27,7 +41,8 @@ describe('CalculatorService', () => {
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     });
 
-    it('should subtract two numbers', () => {
+    //Add x before any test spec or test suite to disable test of that block
+    xit('should subtract two numbers', () => {
         // const calculator = new CalculatorService(new LoggerService());
 
         const result = calculator.subtract(4, 2);
